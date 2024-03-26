9133186c1d0417b3f239459cc94789a9c4c98af8
Jed Belhaj <jedbelhaj05@gmail.com>
2024-03-26 13:31:42 +0000

    fixed module thingy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,11 @@ module com.fsb.linkedin {
     requires java.sql;
     requires javafx.media;
 
+    opens com.fsb.linkedin.entities to javafx.fxml;
+    opens com.fsb.linkedin.utils to javafx.fxml;
     opens com.fsb.linkedin to javafx.fxml;
     exports com.fsb.linkedin;
+    exports com.fsb.linkedin.entities;
+    exports com.fsb.linkedin.utils;
 
 }
